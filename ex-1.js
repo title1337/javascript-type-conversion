9f5da82d7b8c8d626a0a181c73e4f7467b37f4ed
Tle <title1337@gmail.com>
2026-03-05 19:56:52 +0700

    feat: EX1

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,9 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
+depositAmountFromUser = Number(depositAmountFromUser);
 
 accountBalance = accountBalance + depositAmountFromUser;
 
-let accountBalanceMessage = "Your account balance is " + accountBalance;
+let accountBalanceMessage = `Your account balance is ${accountBalance}`;
 
 console.log(accountBalanceMessage);
